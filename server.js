7617873d53fe31709e88f7669ddd18a7aafe8285
jibril5 <dahmani.jibril@gmail.com>
2026-06-01 15:51:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,34 +3,50 @@ import cors from "cors";
 import { spawn } from "child_process";
 
 const app = express();
+
 app.use(cors());
 
 app.get("/stream", (req, res) => {
 
   const url = req.query.url;
-  if (!url) return res.status(400).send("Missing url");
+
+  if (!url) {
+    return res.status(400).send("Missing url");
+  }
 
   res.setHeader("Content-Type", "video/mp4");
-  
+
   const ffmpeg = spawn("ffmpeg", [
     "-i", url,
-  
+
+    "-map", "0:v:0",
+    "-map", "0:a:0",
+
     "-c:v", "copy",
-  
+
     "-c:a", "aac",
+    "-ac", "2",
+    "-ar", "48000",
     "-b:a", "192k",
-  
-    "-movflags", "frag_keyframe+empty_moov",
-  
-    "-f", "mp4",
+
+    "-movflags",
+    "frag_keyframe+empty_moov+default_base_moof",
+
+    "-f",
+    "mp4",
+
     "pipe:1"
   ]);
 
+  ffmpeg.stderr.on("data", d => {
+    console.log(d.toString());
+  });
+
   ffmpeg.stdout.pipe(res);
 
-  req.on("close", () => ffmpeg.kill("SIGKILL"));
+  req.on("close", () => {
+    ffmpeg.kill("SIGKILL");
+  });
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
-});
+app.listen(process.env.PORT || 3000);
